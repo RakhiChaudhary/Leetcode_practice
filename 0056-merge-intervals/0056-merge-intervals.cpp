859a9d455635883a7bfa0b7c82dfbd661e854413
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2023-08-02 12:20:37 +0530

    Time: 31 ms (95.62%), Space: 18.8 MB (95.59%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<int>temp=intervals[0];
+        for(int i=1;i<n;i++){
+            if(temp[1]>=intervals[i][0])
+            {
+                temp[1]=max(temp[1],intervals[i][1]);
+            }
+            else
+            {
+                ans.push_back(temp);
+                temp = intervals[i];
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+    }
+};
+
+
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+// -----
+//                -----
+//   ---------            
+//                         --------------------
+
+
+// 1. sort
+// 2. iniatialize temp = intervals[0];
+
+// [[3,3],[4,6],[4,1],[15,18]]
+// 
+// temp = [4,6]
+// if(temp[1] >= it[i][0]){
+//     temp[1] = max(it[1], temp[1]);
+// }else{
+//     ans.push_back(temp);
+//     temp= it[i];
+// }
+
+
+
+// INTERVAL => [[2,6],[3,3]] =>[2,6]
