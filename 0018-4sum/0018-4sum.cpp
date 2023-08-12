56e9804369cb2071a45d70cdcbe35854a28561de
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2023-08-12 11:24:40 +0530

    Time: 63 ms (83.12%), Space: 13.2 MB (73.19%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+       sort(nums.begin(),nums.end());
+       vector<vector<int>>ans;
+       for(int i=0;i<n;i++){
+           long long newTarget = target - nums[i];
+           //3 sum code
+           for(int j=i+1;j<n;j++){
+               long long newTarget1 = newTarget - nums[j];
+               int s = j+1;
+               int e=n-1;
+               while(s<e){
+                   if((long long) nums[s]+nums[e]==newTarget1)
+                   {
+                       ans.push_back({nums[s],nums[e],nums[i],nums[j]});
+                       while(s<e && nums[s]==nums[s+1]) s++;
+                        while(s<e && nums[e]==nums[e-1]) e--;
+                       s++;
+                       e--;
+                   }
+                   else if(nums[s]+nums[e]<newTarget1)
+                   {
+                       s++;
+                   }
+                   else
+                   e--;
+
+               
+               }
+               while(j<n-1 && nums[j]==nums[j+1]) j++;
+           }
+            while(i<n-1 && nums[i]==nums[i+1]) i++;
+       }
+       return ans;
+       
+    }
+};
+
+//a+b+c+d=target
+//b+c+d=target-a
