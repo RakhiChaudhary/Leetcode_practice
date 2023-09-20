f8ab3de347bb3e0f7e8c872cc753fe1d211ba110
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2023-09-20 21:07:53 +0530

    Time: 20 ms (49.76%), Space: 12.4 MB (17.71%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(string &text1, string &text2,int ind1,int ind2,vector<vector<int>>&dp){
+        if(ind1==text1.size()||ind2==text2.size()){
+            return 0;
+        }
+        
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+        if(text1[ind1]==text2[ind2]){
+            return dp[ind1][ind2]=1+solve(text1,text2,ind1+1,ind2+1,dp);
+        }
+        else{
+            int choice1=solve(text1,text2,ind1+1,ind2,dp);
+            int choice2=solve(text1,text2,ind1,ind2+1,dp);
+            return dp[ind1][ind2]=max(choice1,choice2);
+        }
+    }
+
+    int minDistance(string text1, string text2) {
+        vector<vector<int>>dp(text1.size(),vector<int>(text2.size(),-1));
+        int ans=solve(text1,text2,0,0,dp);
+        return (text1.size()+text2.size()-2*ans);
+    }
+};
