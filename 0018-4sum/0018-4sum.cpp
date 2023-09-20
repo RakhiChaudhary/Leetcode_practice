cb3f11c49309e86f43be23a8eed559da0427a513
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2023-09-20 08:51:30 +0530

    Time: 66 ms (50.89%), Space: 13.6 MB (37.23%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-       sort(nums.begin(),nums.end());
+       // int n=nums.size();
+       /*sort(nums.begin(),nums.end());
        vector<vector<int>>ans;
        for(int i=0;i<n;i++){
            long long newTarget = target - nums[i];
@@ -33,10 +33,49 @@ public:
            }
             while(i<n-1 && nums[i]==nums[i+1]) i++;
        }
-       return ans;
+       return ans;*/
+sort(nums.begin(),nums.end());
+vector<vector<int>>ans;
+int n=nums.size();
+for(int i=0;i<n;i++){
+    long long t1=target-nums[i];
+    for(int j=i+1;j<n;j++){
+        long long t2=t1-nums[j];
+        int s=j+1;
+        int e=n-1;
+        while(s<e){
+            if((long long)nums[s]+nums[e]==t2){
+                ans.push_back({nums[i],nums[j],nums[s],nums[e]});
+                  while(s<e && nums[s]==nums[s+1]){
+                  s++;
+                   }
+                  while(s<e && nums[e]==nums[e-1]){
+                        e--;
+                    }
+                    s++;
+                    e--;
+            }
+            else if(nums[s]+nums[e]<t2){
+                 s++;
+            }
+            else
+            e--; 
+        }
+        while(j+1<n && nums[j]==nums[j+1]){
+            j++;
+        }
+    }
+     while(i+1<n && nums[i]==nums[i+1]){
+            i++;
+        }
+
+}
+return ans;
        
     }
 };
 
 //a+b+c+d=target
 //b+c+d=target-a
+
+
